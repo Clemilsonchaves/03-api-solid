c6fbbbc68cf3af7d240189d01fd7a1ad5fd963f6
clemilsonchaves <clemilsonchaves.1992@gmail.com>
2025-07-11 05:33:40 -0300

    refactor: remove InvalidCredentialsError class and update related tests

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -1,12 +1,9 @@
-import { describe, it, expect, beforeEach } from 'vitest';
-import { AuthenticateUseCase } from './authenticate';
-import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository';
 import { describe, it, expect } from 'vitest';
+import { AuthenticateUseCase } from './authenticate';
 import { InvalidCredentialsError } from './authenticate';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 let sut: AuthenticateUseCase;
-let usersRepository: InMemoryUsersRepository;
 
 describe('InvalidCredentialsError', () => {
   it('should be instance of Error', () => {
